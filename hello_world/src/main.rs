071ce3951c04345c305dd7dd5a02ce29bf99de51
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-24 18:16:18 +0200

    FIRST <3

---
+++ hello_world/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    println!("The Rust journey starts today. May 24th 2022!");
+    println!("SUPER excited!");
+}
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/kenneth/github/hello_rust/hello_world/target/debug/hello_world: /home/kenneth/github/hello_rust/hello_world/src/main.rs
