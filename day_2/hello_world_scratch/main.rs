e2f15a4aee042deeadf85ea2bc5aced75dd9eb26
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-25 13:48:37 +0200

    day_2/

---
+++ day_2/hello_world_scratch/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+  println!("Hello, world!");
+  println!("Wonder how to do easy calculations");
+  println!("Also wonder how to do basically anything else then strings lol");
+  println!("I'm sure we'll learn it soon!");
+}
