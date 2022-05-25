5695c844be46162ec6ff7ed34e0e4d0304750876
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-25 14:16:06 +0200

    hello_world_scratch/

--- day_2/hello_world_scratch/main.rs
+++ day_2/hello_world_scratch/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-  println!("Hello, world!");
-  println!("Wonder how to do easy calculations");
-  println!("Also wonder how to do basically anything else then strings lol");
-  println!("I'm sure we'll learn it soon!");
+    println!("Hello, world!");
+    println!("Wonder how to do easy calculations");
+    println!("Also wonder how to do basically anything else then strings lol");
+    println!("I'm sure we'll learn it soon!");
+
+    println!("println( and println!( is differente! The ! indicates you are calling a macro
+and not a normal function\n")
 }
