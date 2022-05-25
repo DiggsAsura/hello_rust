f077b9ddeb136b17e3008f09164a1e985ad2466d
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-25 14:39:49 +0200

    hello cargo

---
+++ day_2/hello_cargo/src/main.rs
@@ -0,0 +1,7 @@
+// Cargos version of hello world. The way going forward
+// Now following the online Book/PDF
+
+fn main() {
+    println!("Hello, world!");
+    println!("cargo build")
+}
---
+++ day_2/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/kenneth/github/hello_rust/day_2/hello_cargo/target/debug/hello_cargo: /home/kenneth/github/hello_rust/day_2/hello_cargo/src/main.rs
