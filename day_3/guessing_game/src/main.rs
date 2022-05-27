afcbe3f24eef75364669a5ff436167f2b50f0e4d
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-27 21:50:25 +0200

    guessing_game/

--- day_3/guessing_game/src/main.rs
+++ day_3/guessing_game/src/main.rs
@@ -170,38 +170,38 @@
 // same, ill comment out everything and revwiret all in this section.
 
 
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+//use rand::Rng;
+//use std::cmp::Ordering;
+//use std::io;
 
-fn main() {
+//fn main() {
     // --snip--
-    println!("Guess the number!");
+//    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+//    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+//    println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+//    println!("Please input your guess.");
 
-    let mut guess = String::new();
+//    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+//    io::stdin()
+//        .read_line(&mut guess)
+//        .expect("Failed to read line");
 
 
     // added after the beneth text about changing string to number
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+//    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+//    println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
-}
+//    match guess.cmp(&secret_number) {
+//        Ordering::Less => println!("Too small!"),
+//        Ordering::Greater => println!("Too big!"),
+//        Ordering::Equal => println!("You win!"),
+//    }
+//}
 
 // this does not seem to compile yet. 
 
@@ -232,3 +232,48 @@ fn main() {
 // \n represnts "newlin". (On Windows, pressing enter results in carriage rturn and a new newline, 
 // \r\n). The trim method eliminates \n or \r\n, resulting in just 5
 
+
+//
+//
+// ALLOWING MULTIPLE GUESSES WITH LOOPING
+//
+//
+
+// The loop keyword creates an infinite loop. We'll add a loop to give users more chances at 
+// guessing the number:
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    //println!("The secret number is: {}", secret_number);
+    // literally removed to not blow the game 
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // changed to not panick the program if enter non-number.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            // notice the {}
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
