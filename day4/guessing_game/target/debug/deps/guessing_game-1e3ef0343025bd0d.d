1d2920ff4e605c3ee392d0aec29e5788fedd42b4
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-27 23:05:06 +0200

    cargo clean

--- day4/guessing_game/target/debug/deps/guessing_game-1e3ef0343025bd0d.d
+++
@@ -1,5 +0,0 @@
-/home/kenneth/github/hello_rust/day4/guessing_game/target/debug/deps/guessing_game-1e3ef0343025bd0d: src/main.rs
-
-/home/kenneth/github/hello_rust/day4/guessing_game/target/debug/deps/guessing_game-1e3ef0343025bd0d.d: src/main.rs
-
-src/main.rs:
--- day4/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/kenneth/github/hello_rust/day4/guessing_game/target/debug/guessing_game: /home/kenneth/github/hello_rust/day4/guessing_game/src/main.rs
