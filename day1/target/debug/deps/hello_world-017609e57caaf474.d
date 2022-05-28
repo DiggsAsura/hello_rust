f721001501a08922ac1fc80ee292c4695c36c55a
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-28 11:16:53 +0200

    cargo clean

--- day1/target/debug/deps/hello_world-017609e57caaf474.d
+++
@@ -1,5 +0,0 @@
-/home/kenneth/github/hello_rust/day1/target/debug/deps/hello_world-017609e57caaf474: src/main.rs
-
-/home/kenneth/github/hello_rust/day1/target/debug/deps/hello_world-017609e57caaf474.d: src/main.rs
-
-src/main.rs:
--- day1/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/kenneth/github/hello_rust/day1/target/debug/hello_world: /home/kenneth/github/hello_rust/day1/src/main.rs
