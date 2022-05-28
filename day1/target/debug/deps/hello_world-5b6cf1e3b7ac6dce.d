d7c07fb95c177cd65dcb431f39d9bc95ff523fb9
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-28 10:33:30 +0200

    rearranging a bit

---
+++ day1/target/debug/deps/hello_world-5b6cf1e3b7ac6dce.d
@@ -0,0 +1,5 @@
+/home/kenneth/github/hello_rust/hello_world/target/debug/deps/hello_world-5b6cf1e3b7ac6dce: src/main.rs
+
+/home/kenneth/github/hello_rust/hello_world/target/debug/deps/hello_world-5b6cf1e3b7ac6dce.d: src/main.rs
+
+src/main.rs:
---
+++ day1/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/kenneth/github/hello_rust/day1/target/debug/hello_world: /home/kenneth/github/hello_rust/day1/src/main.rs
