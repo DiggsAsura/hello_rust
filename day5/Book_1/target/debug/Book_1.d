cd14abf0b3bffe6893cf29cf4c41a243144aebd4
DiggsAsura <kenneth.bjerke@gmail.com>
2022-05-28 10:32:17 +0200

    Book_1/ - retrying the first book example

--- day5/Book_1/target/debug/Book_1.d
+++
@@ -1,1 +0,0 @@
-/home/kenneth/github/hello_rust/day5/Book_1/target/debug/Book_1: /home/kenneth/github/hello_rust/day5/Book_1/src/main.rs
--- day5/Book_1/target/debug/deps/Book_1-fc249e31403e65ae.d
+++
@@ -1,5 +0,0 @@
-/home/kenneth/github/hello_rust/day5/Book_1/target/debug/deps/Book_1-fc249e31403e65ae: src/main.rs
-
-/home/kenneth/github/hello_rust/day5/Book_1/target/debug/deps/Book_1-fc249e31403e65ae.d: src/main.rs
-
-src/main.rs:
