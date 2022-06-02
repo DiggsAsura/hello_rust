e3e8768017e6647f5b9539e7602f81885c15e2e5
DiggsAsura <kenneth.bjerke@gmail.com>
2022-06-02 11:56:06 +0200

    c5-13-static_lifetimes/

--- Tour_of_Rust/5_Ownership_and_Borrowing_Data/c5-13-static_lifetimes/src/main.rs
+++ Tour_of_Rust/5_Ownership_and_Borrowing_Data/c5-13-static_lifetimes/src/main.rs
@@ -1,3 +1,19 @@
+static PI: f64 = 3.1415;
+
 fn main() {
-    println!("Hello, world!");
+    // static variables can also be scoped to a function
+    static mut SECRET: &'static str = "swordfish";
+
+    // string literals have a 'static lifetime
+    let msg: &'static str = "Hello World!";
+    let p: &'static f64 = &PI;
+    println!("{} {}", msg, p);
+
+    // You can break some rules, but you must be explicit
+    unsafe {
+        // we can set SECRET to a string literal because it is also 'statc
+        SECRET = "abracadabra";
+        println!("{}", SECRET);
+    }
 }
+
