2f3450984fbb6bb038a7ceee7e1d76424f93cf8c
DiggsAsura <kenneth.bjerke@gmail.com>
2022-06-02 08:30:41 +0200

    c5-10-references_of_references/

--- Tour_of_Rust/5_Ownership_and_Borrowing_Data/c5-10-references_of_references/target/debug/deps/c5_10_references_of_references-a5820d50715a681f.d
+++
@@ -1,5 +0,0 @@
-/home/kenneth/github/hello_rust/Tour_of_Rust/5_Ownership_and_Borrowing_Data/c5-10-references_of_references/target/debug/deps/c5_10_references_of_references-a5820d50715a681f: src/main.rs
-
-/home/kenneth/github/hello_rust/Tour_of_Rust/5_Ownership_and_Borrowing_Data/c5-10-references_of_references/target/debug/deps/c5_10_references_of_references-a5820d50715a681f.d: src/main.rs
-
-src/main.rs:
