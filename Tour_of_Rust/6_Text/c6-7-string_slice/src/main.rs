c6181a815fb1e9163523e3d404ac3b01f746f615
DiggsAsura <kenneth.bjerke@gmail.com>
2022-06-02 20:34:19 +0200

    c6-7-string_slice/

--- Tour_of_Rust/6_Text/c6-7-string_slice/src/main.rs
+++ Tour_of_Rust/6_Text/c6-7-string_slice/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+  let a = "hi crab";
+  println!("{}", a.len());
+  let first_word = &a[0..2];
+  let second_word = &a[3..7];
+  // let half_crab = &a[3..5]; FAILS
+  // Rust does not accept slices of invalid unicode characters
+  println!("{} {}", first_word, second_word);
+
 }
