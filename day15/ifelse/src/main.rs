de35a85b8bc83ff6a12315ef02eabf292b53904e
DiggsAsura <kenneth.bjerke@gmail.com>
2022-06-07 14:58:19 +0200

    if else

---
+++ day15/ifelse/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let n = 20;
+    let x = 5;
+
+    if n < x {
+        println!("{} is less than {}!", n, x);
+    } else {
+        println!("{} is larger then {}", n, x);
+    }
+}
