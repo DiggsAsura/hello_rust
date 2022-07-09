6733f1d738be13ff13c02447e2376637114ded2a
DiggsAsura <kenneth.bjerke@gmail.com>
2022-07-09 18:31:48 +0100

    out_of_office_2

---
+++ tmps/out_of_office_2.rs
@@ -0,0 +1,11 @@
+fn ooo() {
+    let words = ["Out", "of", "office", "due", "to", "jazz", "festival"];
+    for word in words {
+        println!("{}", word.to_uppercase());
+    }
+}
+
+fn main() {
+    ooo();
+}
+
