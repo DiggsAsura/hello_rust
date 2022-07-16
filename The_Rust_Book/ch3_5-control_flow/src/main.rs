9cac9b3dd3c4d23ff53a4a607df514b98819b1c9
DiggsAsura <kenneth.bjerke@gmail.com>
2022-07-16 17:33:08 +0200

    3.5 Control Flow - Multiple else/if + using if in a let statement

--- The_Rust_Book/ch3_5-control_flow/src/main.rs
+++ The_Rust_Book/ch3_5-control_flow/src/main.rs
@@ -32,10 +32,45 @@ fn main() {
     if number != 0 {
         println!("number was something other than zero");
     }
+
+    // Following parts
+    part_two(12);
+    part_three();
 }
 
 
-// Handling Multiple Conditions with else if
-// -------------------------------------------
+// Part 2: Handling Multiple Conditions with else if
+// --------------------------------------------------
+// example handle if/else if/else
 
+fn part_two(number: i32) {
+    //let number: i32 = 6;
+    
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0{
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not devisible by 4, 3 or 2");
+    }
+}
+// Using too many if/else can clutter the code. We'll look at match later for that. 
+
+
+// Part 3: Using if in a let Statement
+// ----------------------------------------
+// Because if is an expression, we can use it on the right side of a let statement to 
+// assign the outcome to a variable:
+
+fn part_three() {
+    let condition = true; // 5, false becoming 6
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value ofnumber is: {number}");
+}
+// let number = if condition { 5 } else { six }
+// This will fail, because variables need one type only. Means both arms need to be same type
+// or it will fail at compile time. 
 
