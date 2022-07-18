dc34ea5bd327c0a6b020804e9d4ad49e993b65af
DiggsAsura <kenneth.bjerke@gmail.com>
2022-07-18 21:04:56 +0200

    Loop labels

--- The_Rust_Book/ch3_5-control_flow/src/main.rs
+++ The_Rust_Book/ch3_5-control_flow/src/main.rs
@@ -38,6 +38,7 @@ fn main() {
     part_three();
     part_four();
     part_five();
+    part_six();
 }
 
 
@@ -124,4 +125,39 @@ fn part_five() {
 // of the loop, we add 1 and the counter variable, and then check wheter the counter is 
 // equal to 10. When it is, we use the break keyword with the value counter * 2. 
 // After the loop, we use a semicolon to end the statement that assigns the value to 
-// result. Finally, we print the value in result, which in this case is 20. 
+// result. Finally, we print the value in result, which in this case is 20.
+
+
+// Part 6: Loop Labels to Disambiguate Between Multiple Loops 
+// --------------------------------------------------------------
+//
+// Loop labels need to start with a single quotation
+
+fn part_six() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+// The outer loop has the label 'counting_up and will count up from 0 to 2. The inner loop 
+// without a label counts down from 10 to 9. The first break that doesn't specify a label
+// will exit the inner loop only. The break 'countin_up; statement will exit the outer
+// loop. 
+
+
